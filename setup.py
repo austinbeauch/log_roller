47aea7e57e21e61c8938321b92021e35e661b654
Austin Beauchamp <austinbeauch@gmail.com>
2018-02-03 22:52:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='log_roller',
       version="0.1",
       description='Log file data processing library',
-      url='https://github.com/stargaser/sip_tpv',
+      url='https://github.com/austinbeauch/log_roller',
       author='Austin Beauchamp',
       author_email='austinbeauch@gmail.com',
       license='MIT',
