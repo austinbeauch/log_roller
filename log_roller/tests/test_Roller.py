3d919e9323e2c411642eb0f1cb5e9aa08ede427f
Austin <austinbeauch@gmail.com>
2018-02-20 21:22:59 -0800

    Update Roller test to comply with new roller init

--- log_roller/tests/test_Roller.py
+++ log_roller/tests/test_Roller.py
@@ -24,32 +24,37 @@ class RollerTest(unittest.TestCase):
 
         self.assertEqual(roller.Roller("~/path/to/log.log").filepath, os.getenv('HOME')+"/path/to/log.log")
 
-    def test_adownload(self):
+    def test_a_download(self):
         with self.assertRaises(NoneError):
             roller.Roller(self.filebad).download()
         with self.assertRaises(HTTPError):
-            roller.Roller(self.filebad, 'https://web.uvic.ca/~austinb/')
+            roller.Roller(self.filebad, url='https://web.uvic.ca/~austinb/')
         with self.assertRaises(ValueError):
-            roller.Roller(self.filebad, 'not/a/server')
+            roller.Roller(self.filebad, url='not/a/server')
 
         self.roller1.download(self.server)
         assert(os.path.exists(self.file1))
 
-    def test_zdelete(self):
-        # zdelete to run last (unit tests run alphabetically)
+    def test_b_parser(self):
+        # Test file to be run locally with numpy.
+        # Commented out to avoid adding numpy as a dependency.
+        # The parser computes variances and means WAY better than I expected. We're talking
+        # numpy mean: 5.029483161648177 ; parser mean: 5.029483161648176
+        # numpy variance: 8.261168000112667 ; parser variance: 8.261168000112676
+        pass
+        # test_parse
+        # stat = self.roller1.parse()
+        # self.assertEqual(round(stat['CPU_current Variance'], 6), 8.261166)
+        # self.assertEqual(round(stat['CPU_current Mean'], 6), 5.029483)
+
+    def test_timeframe(self):
+        self.assertEqual(self.roller1.time_pattern, '\d\d:\d\d.\d\d')
+        self.assertEqual(len(self.roller1.time_range("04:20.04", "04:20.10")), 6)
+
+    def test_z_delete(self):
         self.roller1.download(self.server)
         self.roller1.delete()
         assert(not os.path.exists(self.file1))
-
-    def test_parser(self):
-        self.roller1.download('https://web.uvic.ca/~austinb/')
-        stat = self.roller1.parse()
-        self.assertEqual(round(stat['CPU_current Variance'], 10), 7.9861807559)
-        self.assertEqual(round(stat['CPU_current Mean'], 9), 4.902564777)
-
-    def test_timeframe(self):
-        self.assertEqual(len(self.roller1.time_range("4:20.04", "4:20.10")), 6)
-
 
 if __name__ == '__main__':
     unittest.main()
